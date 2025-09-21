fa466f2194b241888214a5ebe3d78bb9468d99b9
frds03 <ahmad03firdaus@gmail.com>
2025-09-21 20:12:47 +0700

    [firdaus] Second Commit

--- backend/main.go
+++ backend/main.go
@@ -47,7 +47,7 @@ type Employee struct {
 }
 
 func enableCors(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "https://manage-employee-next-go.vercel.app/")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
